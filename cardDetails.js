1fc9c35558e8b6e65365a3480494d74a96cb909b
Danny Dainton <dannydainton@gmail.com>
2017-09-21 10:19:30 +0100

    Update cardDetails.js

--- cardDetails.js
+++ cardDetails.js
@@ -1,22 +1,22 @@
 // Dependencies
 
-var casual   = require('casual');
-var jsonfile = require('jsonfile');
+const casual   = require('casual')
+const jsonfile = require('jsonfile')
  
-var file = 'cardDetails.json'
+const file = 'cardDetails.json'
 jsonfile.spaces = 4
 
 // Random Card Data Set
 
-casual.define('cardDetails', function() {
+casual.define('cardDetails', () => {
     return {
-		Name 		: casual.full_name,
-		Card_Type   : casual.card_type,
-		Card_Number : casual.card_number(casual.card_type),
-		Card_Expiry : casual.card_exp
-    };
-});
+	Name	    : casual.full_name,
+	Card_Type   : casual.card_type,
+	Card_Number : casual.card_number(casual.card_type),
+	Card_Expiry : casual.card_exp
+    }
+})
 
 // Write the result to a json file
 
-jsonfile.writeFile(file, casual.cardDetails);
+jsonfile.writeFile(file, casual.cardDetails)
